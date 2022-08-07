702cdf362ee5a5c8ee1ea4ae751d25cdebfaf308
Brian Drake <bdrakester@gmail.com>
2022-08-06 19:55:07 -0700

    Add first simple functionality

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/run/{cmd}")
+def run_cmd(cmd: str):
+    if cmd == "pwd":
+        result = os.getcwd()
+    else:
+        result = "undefinded"
+    return {"cmd": cmd, "result": result}
+
+
